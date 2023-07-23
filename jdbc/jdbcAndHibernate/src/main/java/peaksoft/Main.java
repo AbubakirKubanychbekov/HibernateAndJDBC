18bba029ff8ec58537fe48cc3ed1c2a87f6cbce8
user <kubanycbekovabubakir@gmail.com>
2023-07-23 22:49:16 +0600

    commit

--- jdbc/jdbcAndHibernate/src/main/java/peaksoft/Main.java
+++ jdbc/jdbcAndHibernate/src/main/java/peaksoft/Main.java
@@ -1,7 +1,17 @@
 package peaksoft;
+import peaksoft.service.UserServiceImpl;
 
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
+
+        UserServiceImpl userService=new UserServiceImpl();
+ //       userService.createUsersTable();
+ //        userService.saveUser("Abubakir","Kubanychbekov", (byte) 25);
+ //        userService.saveUser("Ilon","Musk", (byte) 45);
+  //       userService.saveUser("Jeff","Bezos", (byte) 55);
+  //   userService.removeUserById(2L);
+  //      System.out.printf("",userService.getAllUsers());
+ //       userService.cleanUsersTable();
     }
 }
